c3a1ce883dd63285ebb7a3d0ed5ddb6ee1034ebf
boenfu <1997@boenfu.cn>
2021-05-25 21:07:29 +0800

    Init

--- README.md
+++
@@ -1,1 +0,0 @@
-# borkspace
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.global.css';
+
+const Hello = () => {
+  return <div>hello</div>;
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
